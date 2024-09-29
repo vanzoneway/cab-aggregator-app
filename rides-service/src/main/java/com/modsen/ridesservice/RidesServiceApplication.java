8ff140e0882b475e6e1a69cd3d287aeac90065a3
vanzoneway <vanz.evergarden00@gmail.com>
2024-09-29 13:56:54 +0300

    initial commit

---
+++ rides-service/src/main/java/com/modsen/ridesservice/RidesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.modsen.ridesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RidesServiceApplication.class, args);
+    }
+
+}
---
+++ rides-service/src/test/java/com/modsen/ridesservice/RidesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modsen.ridesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
