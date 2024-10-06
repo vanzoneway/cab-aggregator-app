b3bcce52cbd1cb3120798089c5109472f7d31c67
vanzoneway <vanz.evergarden00@gmail.com>
2024-10-06 15:28:24 +0300

    Created db migration and model for service

---
+++ rides-service/src/main/java/com/modsen/ridesservice/model/Ride.java
@@ -0,0 +1,50 @@
+package com.modsen.ridesservice.model;
+
+import com.modsen.ridesservice.model.enums.RideStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Ride {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long driverId;
+
+    @Column(nullable = false)
+    private Long passengerId;
+
+    @Column(nullable = false)
+    private String departureAddress;
+
+    @Column(nullable = false)
+    private String destinationAddress;
+
+    @Enumerated(EnumType.STRING)
+    private RideStatus rideStatus;
+
+    @Column(nullable = false)
+    private LocalDateTime orderDateTime;
+
+    @Column(nullable = false, precision = 5, scale = 2)
+    private BigDecimal cost;
+
+}
---
+++ rides-service/src/main/java/com/modsen/ridesservice/model/enums/RideStatus.java
@@ -0,0 +1,12 @@
+package com.modsen.ridesservice.model.enums;
+
+public enum RideStatus {
+
+    CREATED,
+    ACCEPTED,
+    EN_ROUTE_TO_PASSENGER,
+    EN_ROUTE_TO_DESTINATION,
+    COMPLETED,
+    CANCELED
+
+}
