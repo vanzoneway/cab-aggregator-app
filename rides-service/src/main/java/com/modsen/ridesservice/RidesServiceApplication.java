b184bb29712c09eca096aa3190686de9d1a0cc2d
vanzoneway <vanz.evergarden00@gmail.com>
2024-10-10 18:47:31 +0300

    Created discovery of services

--- rides-service/src/main/java/com/modsen/ridesservice/RidesServiceApplication.java
+++ rides-service/src/main/java/com/modsen/ridesservice/RidesServiceApplication.java
@@ -2,8 +2,10 @@ package com.modsen.ridesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RidesServiceApplication {
 
     public static void main(String[] args) {
