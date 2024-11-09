c3dd85076da2fc1a435bc91c4d6f59324e222380
vanzoneway <vanz.evergarden00@gmail.com>
2024-11-09 16:16:40 +0300

    Added integration-tests for passenger-service

---
+++ passenger-service/src/test/java/com/modsen/passengerservice/controller/impl/PassengerControllerIntegrationTest.java
@@ -0,0 +1,121 @@
+package com.modsen.passengerservice.controller.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.modsen.passengerservice.AppTestUtil;
+import com.modsen.passengerservice.kafka.KafkaConsumerListener;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static com.modsen.passengerservice.IntegrationTestData.PASSENGER_ID;
+import static com.modsen.passengerservice.IntegrationTestData.PASSENGER_PAGE_GET_DTO;
+import static com.modsen.passengerservice.IntegrationTestData.PASSENGER_REQUEST_CREATE_DTO;
+import static com.modsen.passengerservice.IntegrationTestData.PASSENGER_REQUEST_UPDATE_DTO;
+import static com.modsen.passengerservice.IntegrationTestData.PASSENGER_RESPONSE_CREATE_DTO;
+import static com.modsen.passengerservice.IntegrationTestData.PASSENGER_RESPONSE_GET_DTO;
+import static com.modsen.passengerservice.IntegrationTestData.PASSENGER_RESPONSE_UPDATE_DTO;
+import static com.modsen.passengerservice.IntegrationTestData.SQL_DELETE_ALL_DATA;
+import static com.modsen.passengerservice.IntegrationTestData.SQL_INSERT_DATA;
+import static com.modsen.passengerservice.IntegrationTestData.SQL_RESTART_SEQUENCES;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@MockBean({KafkaConsumerListener.class})
+@Sql(statements = {
+        SQL_DELETE_ALL_DATA,
+        SQL_RESTART_SEQUENCES,
+        SQL_INSERT_DATA
+}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+class PassengerControllerIntegrationTest {
+
+    private static final String POSTGRESQL_IMAGE_NAME = "postgres:latest";
+
+    @ServiceConnection
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(DockerImageName
+            .parse(POSTGRESQL_IMAGE_NAME));
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.port = this.port;
+    }
+
+    @Test
+    void createPassenger_ReturnsNewPassengerDto_ContainsAllMandatoryFields() throws Exception {
+        given()
+                    .contentType(ContentType.JSON)
+                    .body(objectMapper.writeValueAsString(PASSENGER_REQUEST_CREATE_DTO))
+                .when()
+                    .post(AppTestUtil.PASSENGER_ENDPOINT)
+                .then()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .contentType(ContentType.JSON)
+                    .body(equalTo(objectMapper.writeValueAsString(PASSENGER_RESPONSE_CREATE_DTO)));
+    }
+
+    @Test
+    void getPagePassengers_ReturnsPageWithPassengerDto_WithDefaultOffsetAndLimit() throws Exception {
+        given()
+                .when()
+                    .get(AppTestUtil.PASSENGER_ENDPOINT)
+                .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .contentType(ContentType.JSON)
+                    .body(equalTo(objectMapper.writeValueAsString(PASSENGER_PAGE_GET_DTO)));
+    }
+
+    @Test
+    void updatePassengerById_ReturnsUpdatedPassengerDto_WithUpdatedPhoneField() throws Exception {
+        given()
+                    .contentType(ContentType.JSON)
+                    .body(objectMapper.writeValueAsString(PASSENGER_REQUEST_UPDATE_DTO))
+                .when()
+                    .put(AppTestUtil.PASSENGER_UPDATE_DELETE_ENDPOINT, PASSENGER_ID)
+                .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .contentType(ContentType.JSON)
+                    .body(equalTo(objectMapper.writeValueAsString(PASSENGER_RESPONSE_UPDATE_DTO)));
+    }
+
+    @Test
+    void safeDeletePassenger_ReturnsNoContentStatusCode_IfDatabaseContainsSuchPassengerId() {
+        given()
+                .when()
+                    .delete(AppTestUtil.PASSENGER_UPDATE_DELETE_ENDPOINT, PASSENGER_ID)
+                .then()
+                    .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    void getPassengerById_ReturnsPassengerDto_IfDatabaseContainsSuchPassengerId() throws Exception {
+        given()
+                .when()
+                    .get(AppTestUtil.PASSENGER_UPDATE_DELETE_ENDPOINT, PASSENGER_ID)
+                .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .contentType(ContentType.JSON)
+                    .body(equalTo(objectMapper.writeValueAsString(PASSENGER_RESPONSE_GET_DTO)));
+    }
+
+}
