fe5280f62afd158c0095a5b75656cdc49261883d
vanzoneway <vanz.evergarden00@gmail.com>
2024-11-28 17:03:08 +0300

    Some fixes

--- rides-service/src/main/java/com/modsen/ridesservice/controller/impl/RideController.java
+++ rides-service/src/main/java/com/modsen/ridesservice/controller/impl/RideController.java
@@ -10,7 +10,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -25,7 +24,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/rides")
 @RequiredArgsConstructor
-@Slf4j
 public class RideController implements RideOperations {
 
     private final RideService rideService;
@@ -63,7 +61,6 @@ public class RideController implements RideOperations {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public RideResponseDto createRide(@Valid @RequestBody RideRequestDto rideRequestDto) {
-        log.info("Creating ride {}", rideRequestDto);
         return rideService.createRide(rideRequestDto);
     }
 
