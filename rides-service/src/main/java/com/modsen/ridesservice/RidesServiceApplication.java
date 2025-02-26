545521a2781db3d773b017a10ffbe3aa9c2ad9ad
vanzoneway <vanz.evergarden00@gmail.com>
2025-02-26 15:56:51 +0300

    Fix tests for rides-service
    1. Fix integration-tests
    2. Fix unit-tests
    3. Fix e2e-tests

--- rides-service/src/main/java/com/modsen/ridesservice/RidesServiceApplication.java
+++ rides-service/src/main/java/com/modsen/ridesservice/RidesServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableMethodSecurity
+@EnableMethodSecurity(proxyTargetClass = true)
 @EnableCaching
 public class RidesServiceApplication {
 
--- rides-service/src/test/java/com/modsen/ridesservice/e2e/E2ETestData.java
+++ rides-service/src/test/java/com/modsen/ridesservice/e2e/E2ETestData.java
@@ -7,8 +7,13 @@ import lombok.NoArgsConstructor;
 public final class E2ETestData {
 
     public static final String BASE_URL = "http://localhost:8080/api/v1/rides";
+    public static final String ADMIN_AUTH_TOKEN_URL = "http://localhost:8080/api/v1/cab-aggregator/signin/admin";
     public static final String UPDATE_RIDE_STATUS_POSTFIX = "/{id}/status";
     public static final String RIDE_ID_POSTFIX = "/{id}";
+
+    public static final String GRANT_TYPE = "client_credentials";
+    public static final String CLIENT_ID = "admin-cli";
+    public static final String CLIENT_SECRET = "VW0msamXP7l5NARSzDYwE22I8IF9s7Jo";
 
     public static final String ORDER_DATE_TIME_FIELD = "orderDateTime";
     public static final String COST_FIELD = "cost";
