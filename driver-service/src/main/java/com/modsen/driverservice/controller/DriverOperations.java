05f186bb537319c6738e7dc052d71d561c929cc7
vanzoneway <vanz.evergarden00@gmail.com>
2025-02-28 10:29:22 +0300

    Documented some of driver operations

--- driver-service/src/main/java/com/modsen/driverservice/controller/DriverOperations.java
+++ driver-service/src/main/java/com/modsen/driverservice/controller/DriverOperations.java
@@ -5,9 +5,6 @@ import com.modsen.driverservice.dto.DriverDto;
 import com.modsen.driverservice.dto.ListContainerResponseDto;
 import com.modsen.driverservice.dto.Marker;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
@@ -19,100 +16,136 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Validated
-@Tag(name = "Driver controller", description = """
-         This controller contains endpoints for creating a driver, updating driver information,\s
-         performing a soft delete of a driver, and retrieving information\s
-         about all drivers.
-        \s""")
+@Tag(name = "car-operations", description = """
+        The endpoints contained here are intended for operations related to drivers. For example: creating a driver,
+        retrieving a list of drivers, deleting drivers, updating a driver by ID, retrieving a specific driver
+        by ID, and getting a driver along with their list of cars.
+
+        It is important to note that JWT authorization is used here: ROLE_ADMIN can perform all actions, while
+        ROLE_DRIVER can only perform actions related to themselves (they cannot modify information of other drivers).
+
+        Additionally, please be aware that when deleting or updating a driver, the data is not synchronized
+        with the registration service database (THIS HAS NOT BEEN FIXED YET). This should be taken into account.
+        """)
 public interface DriverOperations {
 
-    @Operation(summary = "Create a new driver",
+    @Operation(summary = "Creates a new driver",
             description = """
-                    Required fields:\s
-                    - **name**: Name of the driver (non-empty string)
-                    - **email**: Valid email address
-                    - **phone**: Phone number in valid format
-                    - **age**: Age of the driver (must be 21 or older)
-                    - **gender**: Gender of the driver (non-empty string)
-                    Example:\s
+                    The driver details to be created. Please note that the data is not yet synchronized 
+                    with the registration service database. Required fields:
+                    - **firstName**: First name of the driver (non-empty string)
+                    - **lastName**: Last name of the driver (non-empty string)
+                    - **email**: Email address of the driver (valid format)
+                    - **phone**: Phone number of the driver (valid format)
+                    - **gender**: Gender of the driver (e.g., 'Male', 'Female')
+
+                    Validation rules:
+                    - `firstName` and `lastName` must not be blank.
+                    - `email` must be a valid email format and not empty.
+                    - `phone` must match the pattern: +[country code][number] (7-15 digits total).
+                    - `gender` must not be blank.
+
+                    Example for creating a driver:
                     {
-                      "name": "Jane Doe",
-                      "email": "janedoe@example.com",
-                      "phone": "+1234567890",
-                      "age": 30,
-                      "gender": "Female"
-                    }""")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Driver created successfully"),
-            @ApiResponse(responseCode = "400", description = "Invalid input provided"),
-            @ApiResponse(responseCode = "409", description = "A conflict occurred. This happens when such a driver " +
-                    "email or phone already exists")
-    })
+                        "firstName": "John",
+                        "lastName": "Lock",
+                        "email": "johnddoe@example.com",
+                        "phone": "+1234567503",
+                        "gender": "Male"
+                    }
+
+                    Example of a driver object after creation:
+                    {
+                        "id": 34,
+                        "firstName": "John",
+                        "lastName": "Lock",
+                        "email": "johnddoe@example.com",
+                        "phone": "+1234567503",
+                        "gender": "Male",
+                        "averageRating": null,
+                        "deleted": false
+                    }
+                    """)
     @Validated(Marker.OnCreate.class)
     DriverDto createDriver(
-            @Parameter(description = "Driver details to be created", required = true)
             @RequestBody @Valid DriverDto driverDto);
 
-    @Operation(summary = "Get paginated list of drivers",
-            description = "Retrieves a paginated list of drivers.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Drivers retrieved successfully"),
-            @ApiResponse(responseCode = "400", description = "Invalid pagination parameters")
-    })
+    @Operation(summary = "Retrieves a paginated list of drivers",
+            description = """
+                    Retrieves a list of drivers with pagination support. The results are returned in a paginated format.
+                    Required query parameters:
+                    - **offset**: The starting point of the results (default is 0, must be >= 0).
+                    - **limit**: The maximum number of results to return (default is 10, must be between 1 and 100).
+
+                    Example request:
+                    GET http://localhost:8080/api/v1/drivers?offset=0&limit=2
+
+                    Example response:
+                    {
+                        "currentOffset": 0,
+                        "currentLimit": 2,
+                        "totalPages": 11,
+                        "totalElements": 21,
+                        "sort": "UNSORTED",
+                        "values": [
+                            {
+                                "id": 1,
+                                "firstName": "John",
+                                "lastName": "Doe",
+                                "email": "johndoe@example.com",
+                                "phone": "+1234567890",
+                                "gender": "Male",
+                                "averageRating": null,
+                                "deleted": false
+                            },
+                            {
+                                "id": 2,
+                                "firstName": "Jane",
+                                "lastName": "Smith",
+                                "email": "janesmith@example.com",
+                                "phone": "+1234567891",
+                                "gender": "Female",
+                                "averageRating": null,
+                                "deleted": false
+                            }
+                        ]
+                    }
+                    """)
     ListContainerResponseDto<DriverDto> getPageDrivers(
-            @Parameter(description = "Offset for pagination", example = "0")
             @RequestParam(defaultValue = "0") @Min(0) Integer offset,
-            @Parameter(description = "Limit for pagination", example = "10")
             @RequestParam(defaultValue = "10") @Min(1) @Max(100) Integer limit);
 
-    @Operation(summary = "Soft delete a driver",
-            description = "Marks the specified driver as deleted.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Driver deleted successfully"),
-            @ApiResponse(responseCode = "404", description = "Driver not found")
-    })
+    @Operation(summary = "Safely deletes a driver",
+            description = """
+                    Safely deletes a driver by their ID. This operation ensures that the driver's data is marked as deleted 
+                    without permanently removing it from the database. This allows for potential data recovery if needed.
+                    
+                    Required path variable:
+                    - **driverId**: The ID of the driver to be deleted (must be a valid ID).
+
+                    Authorization:
+                    - This operation requires JWT authentication. Only users with the appropriate roles can perform this action.
+
+                    Example request:
+                    DELETE http://localhost:8080/api/v1/drivers/{driverId}
+
+                    Example response:
+                    - HTTP 204 No Content: Indicates that the driver was successfully deleted.
+                    """)
     void safeDeleteDriver(
-            @Parameter(description = "ID of the driver to be deleted", required = true)
             @PathVariable Long driverId,
             JwtAuthenticationToken jwtAuthenticationToken);
 
-    @Operation(summary = "Update driver information",
-            description = "Updates the details of the specified driver.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Driver updated successfully"),
-            @ApiResponse(responseCode = "404", description = "Driver not found"),
-            @ApiResponse(responseCode = "400", description = "Invalid input provided"),
-            @ApiResponse(responseCode = "409", description = "A conflict occurred. This happens when such a driver " +
-                    "email or phone already exists")
-    })
+
     @Validated(Marker.OnUpdate.class)
     DriverDto updateDriverById(
-            @Parameter(description = "ID of the driver to be updated", required = true)
             @PathVariable Long driverId,
-            @Parameter(description = "Updated driver details. It is not necessary to specify all parameters. It is " +
-                    "sufficient to provide just a subset of them.",
-                    required = true)
             @RequestBody @Valid DriverDto driverDto,
             JwtAuthenticationToken jwtAuthenticationToken);
 
-    @Operation(summary = "Get driver by id",
-            description = "Getting information about driver in JSON format")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Driver got successfully"),
-            @ApiResponse(responseCode = "404", description = "Driver not found"),
-            @ApiResponse(responseCode = "400", description = "Invalid input provided"),
-
-    })
     DriverDto getDriverById(@PathVariable Long driverId);
 
-    @Operation(summary = "Get driver with all associated cars",
-            description = "Retrieves the driver information along with all cars associated with them.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Driver and cars retrieved successfully"),
-            @ApiResponse(responseCode = "404", description = "Driver not found")
-    })
-    DriverCarDto getDriverWithCars(
-            @Parameter(description = "ID of the driver", required = true) @PathVariable Long driverId);
+    DriverCarDto getDriverWithCars(@PathVariable Long driverId);
 
 }
 
--- driver-service/src/main/java/com/modsen/driverservice/dto/DriverDto.java
+++ driver-service/src/main/java/com/modsen/driverservice/dto/DriverDto.java
@@ -10,14 +10,9 @@ import lombok.Builder;
 
 import java.io.Serializable;
 
-/**
- * DTO for {@link com.modsen.driverservice.model.Driver Driver}
- */
-@Schema(description = "Data Transfer Object for Driver")
 @Builder(setterPrefix = "with")
 public record DriverDto(
         @NotNull(groups = Marker.OnGet.class)
-        @Schema(description = "Unique identifier for the driver", example = "1")
         Long id,
 
         @NotBlank(message = "{firstname.empty}", groups = Marker.OnCreate.class)
@@ -29,7 +24,6 @@ public record DriverDto(
         String lastName,
 
         @Email(message = "{email.invalid}", groups = {Marker.OnCreate.class, Marker.OnUpdate.class})
-        @Schema(description = "Email address of the driver", example = "janedoe@example.com")
         @NotBlank(message = "{email.empty}", groups = Marker.OnCreate.class)
         @Null(groups = Marker.OnUpdate.class)
         String email,
@@ -37,18 +31,15 @@ public record DriverDto(
         @Pattern(message = "{phone.invalid}",
                 regexp = "^\\+?[1-9][0-9]{7,14}$",
                 groups = {Marker.OnCreate.class, Marker.OnUpdate.class})
-        @Schema(description = "Phone number of the driver", example = "+1234567890")
         @NotBlank(message = "{phone.empty}", groups = Marker.OnCreate.class)
         String phone,
 
         @NotBlank(message = "{gender.empty}", groups = Marker.OnCreate.class)
-        @Schema(description = "Gender of the driver", example = "Female")
         @Null(groups = Marker.OnUpdate.class)
         String gender,
 
         @Null(groups = {Marker.OnCreate.class, Marker.OnUpdate.class})
         Double averageRating,
 
-        @Schema(hidden = true)
         Boolean deleted) implements Serializable {
 }
