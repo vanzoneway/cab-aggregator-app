c537e10cb1798aacb9eda62ba4ab13af3091b617
vanzoneway <vanz.evergarden00@gmail.com>
2025-03-11 20:51:33 +0300

    Some fixes

--- passenger-service/src/main/java/com/modsen/passengerservice/configuration/OpenApiConfig.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/configuration/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -14,7 +15,10 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
                         name = "Ivan Zinovich",
                         email = "vanz.evergarden0@gmail.com"
                 )
-        )
+        ),
+        servers = {
+                @Server(url = "http://localhost:8086", description = "Local server")
+        }
 )
 @SecurityScheme(
         name = "bearerAuth",
--- registration-service/src/main/java/com/modsen/registrationservice/configuration/OpenApiConfig.java
+++ registration-service/src/main/java/com/modsen/registrationservice/configuration/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.modsen.registrationservice.configuration;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -12,7 +13,10 @@ import io.swagger.v3.oas.annotations.info.Info;
                         name = "Ivan Zinovich",
                         email = "vanz.evergarden0@gmail.com"
                 )
-        )
+        ),
+        servers = {
+                @Server(url = "http://localhost:8089", description = "Local server")
+        }
 )
 public class OpenApiConfig {
 }
